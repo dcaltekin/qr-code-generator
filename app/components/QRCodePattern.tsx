9615e5b3309e7ff240029e79b905e772eee5e329
dcaltekin <dcaltekin@gmail.com>
2023-07-27 11:40:12 +0300

    ADDED: tabs-ui

--- app/components/QRCodePattern.tsx
+++ app/components/QRCodePattern.tsx
@@ -3,10 +3,12 @@
 import React from "react";
 import QRCode from "qrcode.react";
 import DownloadButton from "./DownloadButton";
+import Tabs from "./Tabs";
 
 export default function QRCodePattern() {
   return (
     <div className="flex justify-center items-center flex-col h-screen gap-y-4">
+      <Tabs />
       <QRCode
         id="qr-code-svg"
         value="https://dogukancaltekin.vercel.app/"
---
+++ app/components/Tabs.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+import { BsPersonVcard, BsQrCodeScan } from "react-icons/bs";
+
+export default function Tabs() {
+  return (
+    <div>
+      <div className="border-b border-gray-600 ">
+        <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 ">
+          <li className="mr-2">
+            <button className="flex gap-x-2 items-center justify-center p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active  group">
+              <BsQrCodeScan size={20} />
+              QR Card
+            </button>
+          </li>
+          <li className="mr-2">
+            <button className="flex items-center gap-x-2  p-4 text-gray-400 rounded-t-lg cursor-not-allowed ">
+              <BsPersonVcard size={20} />
+              VCard (Coming soon)
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
