f5f21c0bb9c299e05f8cc30fbf47b5455c20b40b
dcaltekin <dcaltekin@gmail.com>
2023-07-27 16:32:22 +0300

    UPDATE: tabs-padding

--- app/components/Tabs.tsx
+++ app/components/Tabs.tsx
@@ -9,13 +9,13 @@ export default function Tabs() {
       <div className="border-b border-gray-600 ">
         <ul className="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 ">
           <li className="mr-2">
-            <button className="flex gap-x-2 items-center justify-center p-4 text-blue-500 border-b-2 border-blue-500 rounded-t-lg active  group">
+            <button className="flex gap-x-2 items-center justify-center px-4 pb-4 text-blue-500 border-b-2 border-blue-500 rounded-t-lg active  group">
               <BsQrCodeScan size={20} />
               QR Card
             </button>
           </li>
           <li className="mr-2">
-            <button className="flex items-center gap-x-2  p-4 text-gray-400 rounded-t-lg cursor-not-allowed ">
+            <button className="flex items-center gap-x-2  px-4 pb-4 text-gray-400 rounded-t-lg cursor-not-allowed ">
               <BsPersonVcard size={20} />
               VCard (Coming soon)
             </button>
